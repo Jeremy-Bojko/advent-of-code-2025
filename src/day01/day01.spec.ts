43156aa1ec3fa4acb3ccdb5e5a0df075e5b9b20b
Jérémy Bojko macMini <jeremy.bojko@protonmail.com>
2025-12-03 09:28:46 +0100

    try(day01): part 2

--- src/day01/day01.spec.ts
+++ src/day01/day01.spec.ts
@@ -1,22 +1,64 @@
-import { findNewPosition } from "./main";
+import { findNewPosition, countPassingByZero } from "./main";
 
 describe("Day 1", () => {
-  it("rotate from 1 position to 0", () => {
+  it("rotates from 1 position to 0", () => {
     expect(findNewPosition({ initial: 1, rotationDirection: -1, rotationStep: 1 })).toBe(0);
   });
-  it("rotate from 1 position to 2", () => {
+  it("rotates from 1 position to 2", () => {
     expect(findNewPosition({ initial: 1, rotationDirection: 1, rotationStep: 1 })).toBe(2);
   });
-  it("rotate from 50 position to 82", () => {
+  it("rotates from 50 position to 82", () => {
     expect(findNewPosition({ initial: 50, rotationDirection: -1, rotationStep: 68 })).toBe(82);
   });
-  it("rotate from 50 position to 50", () => {
+  it("rotates from 50 position to 50", () => {
     expect(findNewPosition({ initial: 50, rotationDirection: -1, rotationStep: 100 })).toBe(50);
   });
-  it("rotate from 50 position to 50", () => {
+  it("rotates from 50 position to 50", () => {
     expect(findNewPosition({ initial: 50, rotationDirection: 1, rotationStep: 100 })).toBe(50);
   });
-  it("rotate from 50 position to 0", () => {
+  it("rotates from 50 position to 0", () => {
     expect(findNewPosition({ initial: 50, rotationDirection: 1, rotationStep: 50 })).toBe(0);
+  });
+  it("finds that we passed through zero", () => {
+    const initial = 1,
+      rotationDirection = -1,
+      rotationStep = 1,
+      expected = 0;
+    expect(countPassingByZero({ initial, rotationDirection, rotationStep, newPosition: findNewPosition({ initial, rotationDirection, rotationStep }) })).toBe(expected);
+  });
+  it("finds that we passed through zero", () => {
+    const initial = 1,
+      rotationDirection = -1,
+      rotationStep = 2,
+      expected = 1;
+    expect(countPassingByZero({ initial, rotationDirection, rotationStep, newPosition: findNewPosition({ initial, rotationDirection, rotationStep }) })).toBe(expected);
+  });
+  it("finds that we passed through zero clockwise", () => {
+    const initial = 99,
+      rotationDirection = 1,
+      rotationStep = 2,
+      expected = 1;
+    expect(countPassingByZero({ initial, rotationDirection, rotationStep, newPosition: findNewPosition({ initial, rotationDirection, rotationStep }) })).toBe(expected);
+  });
+  it("finds that we passed multiple time through zero", () => {
+    const initial = 1,
+      rotationDirection = -1,
+      rotationStep = 200,
+      expected = 2;
+    expect(countPassingByZero({ initial, rotationDirection, rotationStep, newPosition: findNewPosition({ initial, rotationDirection, rotationStep }) })).toBe(expected);
+  });
+  it("finds that we passed multiple time through zero", () => {
+    const initial = 0,
+      rotationDirection = -1,
+      rotationStep = 201,
+      expected = 2;
+    expect(countPassingByZero({ initial, rotationDirection, rotationStep, newPosition: findNewPosition({ initial, rotationDirection, rotationStep }) })).toBe(expected);
+  });
+  it("finds not passing through zero", () => {
+    const initial = 0,
+      rotationDirection = -1,
+      rotationStep = 1,
+      expected = 0;
+    expect(countPassingByZero({ initial, rotationDirection, rotationStep, newPosition: findNewPosition({ initial, rotationDirection, rotationStep }) })).toBe(expected);
   });
 });
--- src/day01/main.ts
+++ src/day01/main.ts
@@ -3,27 +3,31 @@ import * as utilities from "../utilities";
 const inputData = `${__dirname}/inputData.txt`;
 const testData = `${__dirname}/testData.txt`;
 
-const isTest = false;
-const data = isTest ? testData : inputData;
+const param = {
+  isTest: false,
+  part: 2,
+};
+const data = param.isTest ? testData : inputData;
 
 /**
  * Part 1
  */
+if (param.part === 1) {
+  utilities.fetchAndTest(data).then((data: string) => {
+    const safePositions = [50];
 
-utilities.fetchAndTest(data).then((data: string) => {
-  const safePositions = [50];
+    const rotations = utilities.parseListToTab(data).map((x) => {
+      return { direction: x.slice(0, 1) === "L" ? -1 : 1, step: +x.slice(1) };
+    });
 
-  const rotations = utilities.parseListToTab(data).map((x) => {
-    return { direction: x.slice(0, 1) === "L" ? -1 : 1, step: +x.slice(1) };
+    rotations.forEach((rotate) => {
+      const newPosition = findNewPosition({ initial: safePositions[safePositions.length - 1], rotationDirection: rotate.direction, rotationStep: rotate.step });
+      safePositions.push(newPosition);
+    });
+    console.log(safePositions);
+    console.log(safePositions.filter((x) => x == 0).length);
   });
-
-  rotations.forEach((rotate) => {
-    const newPosition = findNewPosition({ initial: safePositions[safePositions.length - 1], rotationDirection: rotate.direction, rotationStep: rotate.step });
-    safePositions.push(newPosition);
-  });
-  console.log(safePositions);
-  console.log(safePositions.filter((x) => x == 0).length);
-});
+}
 
 export const findNewPosition = ({ initial = 50, rotationDirection = -1, rotationStep = 100 }) => {
   let newPosition = initial + rotationDirection * (rotationStep % 100);
@@ -31,4 +35,45 @@ export const findNewPosition = ({ initial = 50, rotationDirection = -1, rotation
     newPosition += 100;
   }
   return newPosition % 100;
+};
+
+/**
+ * Part 2
+ */
+if (param.part == 2) {
+  utilities.fetchAndTest(data).then((data: string) => {
+    const safePositions = [50];
+    let totalZero = 0;
+
+    const rotations = utilities.parseListToTab(data).map((x) => {
+      return { direction: x.slice(0, 1) === "L" ? -1 : 1, step: +x.slice(1) };
+    });
+
+    rotations.forEach((rotate) => {
+      const initial = safePositions[safePositions.length - 1],
+        rotationDirection = rotate.direction,
+        rotationStep = rotate.step;
+
+      const newPosition = findNewPosition({ initial, rotationDirection, rotationStep });
+      totalZero += countPassingByZero({ initial, rotationDirection, rotationStep, newPosition });
+      console.log(`step: ${rotationStep}, zero: ${totalZero}, newPos: ${newPosition}`);
+      safePositions.push(newPosition);
+    });
+    // console.log(safePositions);
+    console.log(safePositions.filter((x) => x == 0).length + totalZero);
+  });
+}
+
+export const countPassingByZero = ({ initial = 50, rotationDirection = -1, rotationStep = 100, newPosition = 50 }) => {
+  const completeTour = Math.floor(rotationStep / 100);
+  if (completeTour) {
+    return completeTour;
+  } else if (rotationDirection == -1 && initial < newPosition) {
+    if (initial === 0) return 0;
+    return 1;
+  } else if (rotationDirection == 1 && initial > newPosition) {
+    if (initial === 0) return 0;
+    return 1;
+  }
+  return 0;
 };
